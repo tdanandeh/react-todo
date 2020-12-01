c95ddddeccfd022c805130830f858378686731b4
Tohid Danandeh <t.danandeh@gmail.com>
2020-12-01 07:18:54 -0800

    Updates

---
+++ precache-manifest.b7a355d0f35c32c102fa4b39a4387805.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ab4cb261891bf21ce0c",
+    "url": "/react-todo/static/css/main.c7bb5e0f.chunk.css"
+  },
+  {
+    "revision": "3ab4cb261891bf21ce0c",
+    "url": "/react-todo/static/js/main.3ab4cb26.chunk.js"
+  },
+  {
+    "revision": "4138f94bf984f5790c9d",
+    "url": "/react-todo/static/js/1.4138f94b.chunk.js"
+  },
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "2eb1e687dcd767aa8d554adeae5faec9",
+    "url": "/react-todo/index.html"
+  }
+];
